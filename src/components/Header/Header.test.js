578ee2c172db324249ba560378d79fb64ce834e0
Josh Bryson <josh@bryson.me.uk>
2020-09-10 15:13:54 +0100

    Initial Cypress setup

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Header from './Header';
+import Menu from './Menu/Menu';
 
 test('renders the Header component', () => {
-    const title = 'React CryptoCoin Plotly';
-    render(<Header />);
+    const title = 'React Plotly';
+    render(<Menu />);
     expect(screen.getByText(title)).toBeInTheDocument();
 });
