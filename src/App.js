8b7fbd3bab1125c458d27240ef80ea11d7eceaf9
sleeplesseditor <josh@bryson.me.uk>
2021-04-15 17:19:10 +0100

    Redux implementation setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import LazyLoader from './components/LazyLoader/LazyLoader';
 import ErrorBoundary from './pages/ErrorBoundary';
 import './App.scss';
 import Menu from './components/Menu/Menu';
 import { ReactComponent as CaretIcon } from './components/Menu/Icons/caret.svg';
+import {Provider} from 'react-redux';
+import store from "./store";
 
 const LinePage = React.lazy(() => import('./pages/LinePage/LinePage'));
 const PieChartPage = React.lazy(() => import('./pages/PieChartPage/PieChartPage'));
@@ -14,18 +16,21 @@ const GDPChoroplethMapPage = React.lazy(() => import('./pages/GDPChoroplethMapPa
 
 function App() {
   return (
-    <Router>
-      <Menu navIcon={<CaretIcon />} title={'React Plotly'} />
-      <ErrorBoundary>
-        <Switch>
-          <Route exact path="/" component={LazyLoader(LinePage)} />
-          <Route exact path="/pie-chart" component={LazyLoader(PieChartPage)} />
-          <Route exact path="/switch-graph" component={LazyLoader(SwitchGraphPage)} />
-          <Route exact path="/surface-plot" component={LazyLoader(SurfacePlotPage)} />
-          <Route exact path="/gapminder-map" component={LazyLoader(GDPChoroplethMapPage)} />
-        </Switch>
-      </ErrorBoundary>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Menu navIcon={<CaretIcon />} title={'React Plotly'} />
+        <ErrorBoundary>
+          <Switch>
+            <Route exact path="/" component={LazyLoader(LinePage)} />
+            <Route exact path="/pie-chart" component={LazyLoader(PieChartPage)} />
+            <Route exact path="/switch-graph" component={LazyLoader(SwitchGraphPage)} />
+            <Route exact path="/surface-plot" component={LazyLoader(SurfacePlotPage)} />
+            <Route exact path="/gapminder-map" component={LazyLoader(GDPChoroplethMapPage)} />
+          </Switch>
+        </ErrorBoundary>
+        <Redirect to="/"/>
+      </Router>
+    </Provider>
   );
 }
 
--- src/components/Menu/Icons/IconSelector.jsx
+++ src/components/Menu/Icons/IconSelector.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { ReactComponent as ArrowIcon } from './arrow.svg';
 import { ReactComponent as BasicIcon } from './basic.svg';
 import { ReactComponent as CubeIcon } from './cube.svg';
+import { ReactComponent as DynamicIcon } from './dynamic.svg';
 import { ReactComponent as GlobalisationIcon } from './globalisation.svg';
 import { ReactComponent as GlobeIcon } from './globeIcon.svg';
 import { ReactComponent as LineIcon } from './lineGraph.svg';
 import { ReactComponent as MapIcon } from './mapIcon.svg';
 import { ReactComponent as MountainIcon } from './mountains.svg';
 import { ReactComponent as PieIcon } from './pieIcon.svg';
+import { ReactComponent as StockIcon } from './stock.svg';
 import { ReactComponent as SwitchIcon } from './switch.svg';
 
 const getIcon = (icon) => {
@@ -15,12 +17,14 @@ const getIcon = (icon) => {
         arrowIcon: () => <ArrowIcon/>,
         basicIcon: () => <BasicIcon/>,
         cubeIcon: () => <CubeIcon/>,
+        dynamic: () => <DynamicIcon/>,
         globalisation: () => <GlobalisationIcon/>,
         globeIcon: () => <GlobeIcon/>,
         lineIcon: () => <LineIcon/>,
         mapIcon: () => <MapIcon/>,
         mountainIcon: () => <MountainIcon/>,
         pieIcon: () => <PieIcon/>,
+        stock: () => <StockIcon/>,
         switchIcon: () => <SwitchIcon/>,
         default: () => null
     }
